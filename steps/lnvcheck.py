a52c90b60586871d3e569afd7ccf0ff5fbdaaa6c
Srivats Iyer <siyer79@gmail.com>
2016-12-16 14:32:10 -0800

    Update lnvcheck.py

--- steps/lnvcheck.py
+++ steps/lnvcheck.py
@@ -13,6 +13,36 @@ def step_impl(context):
 
 @when('VNI is in a bridge?')
 def step_impl(context):
+    status, output = commands.getstatusoutput('grep "vxlan-id" /etc/network/interfaces | nawk \'{print $2}\'')
+
+    # Create an Array with vxlan-id's
+    vxlanids = output.split('\n')
+
+    vnilist = []
+   
+    #  Look in the nearby lines of the vxlan id for "auto" and then add that 
+    #  interface id in array vnilist
+    for vxlanid in vxlanids:
+       status, output = commands.getstatusoutput('grep -C 2 "vxlan-id '+vxlanid+'" /etc/network/interfaces | grep auto | nawk \'{print $2}\'')
+       vnilist.append(output) 
+
+    # Assumes that VNI matches with a bridge-port in interfaces, if it doesn't match below in the for 
+    # loop then val is changed to 1
+    val = 0  
+
+    # Check if VNI is in a bridge-port  
+    for vni in vnilist:
+        status, output = commands.getstatusoutput('grep bridge /etc/network/interfaces | grep '+vni) 
+        # check if output is blank and set val to 1 
+        if not output:
+           val = 1 
+
+    if val != 0:
+       raise NotImplementedError('VNI defined in interfaces is NOT in a Bridge (bridge-ports)!') 
+
+
+@then('is that bridge up?')
+def step_impl(context):
     status, output = commands.getstatusoutput('grep "auto vni" /etc/network/interfaces | nawk \'{print $2}\'')
 
     # Create an Array with list of VNI's set on interfaces file
@@ -33,16 +63,17 @@ def step_impl(context):
            val = 1 
  
     if val != 0:
-       raise NotImplementedError('VNI defined in interfaces is NOT in a Bridge!')
+       raise NotImplementedError('Bridge where VNI is defined is down!')
 
- 
-@then('is that bridge up?')
-def step_impl(context):
-    pass 
 
 @then('are there other ports in the bridge?')
 def step_impl(context):
+    status, output = commands.getstatusoutput('grep "auto vni" /etc/network/interfaces | nawk \'{print $2}\'')
+
+    # Create an Array with list of VNI's set on interfaces file
+    vnilist = output.split('\n')
     pass
+
 
 @then('do I have a route to the remote endpoints?')
 def step_impl(context):
